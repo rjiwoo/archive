e38e05dee1ec7b4469f5e415465d4bf51d96fae4
rjiwoo <ryu-jiwoo@naver.com>
2025-09-24 15:44:24 +0900

    2025.9.24. 수업

---
+++ SSAFY_14/2025/2025.09/0924/04-django-orm/articles/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .models import Article
+
+# Create your views here.
+
+# 전체 게시글 조회(1) 후 메인 페이지 응답(2)
+def index(request):
+    # 1. DB에 전체 게시글을 조회
+    articles = Article.objects.all()
+
+    # 2. 전체 게시글 목록을 템플릿과 함께 응답
+    context = {
+        'articles' : articles,
+    }
+
+    return render(request, 'articles/index.html', context)
+    pass
